a45dc75bc80688237e97c4f1d2c41d903e1f2387
ibrahimshokry <ibrahimshokry333@gmail.com>
2024-05-28 13:40:37 +0300

    add (business & science & sports) screens

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:ibrahim_project/features/home/presentation/views/widgets/bottom_nav_bar.dart';
+import 'package:ibrahim_project/features/home/presentation/views/widgets/business_screen.dart';
 import 'package:ibrahim_project/features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:ibrahim_project/features/home/presentation/views/widgets/science_screen.dart';
+import 'package:ibrahim_project/features/home/presentation/views/widgets/sports_screen.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> screens = [
+      const BusinessScreen(),
+      const ScienceScreen(),
+      const SportsScreen(),
+    ];
     return Scaffold(
-        appBar: customAppBar(),
+      appBar: customAppBar(),
+      body: screens[0],
       bottomNavigationBar: const BottomNavBar(),
     );
   }
---
+++ lib/features/home/presentation/views/widgets/business_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BusinessScreen extends StatelessWidget {
+  const BusinessScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column();
+  }
+}
